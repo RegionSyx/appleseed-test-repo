806f5a3e494542310f7da9bccdeb78e27e8732f1
Eric Hurst <eric.hurst97@gmail.com>
2021-11-28 23:59:49 +0000

    Apply appleseed

--- repos/posts.py
+++ repos/posts.py
@@ -9,11 +9,10 @@ class Post:
 
 
 class PostRepo:
-
     def __init__(self, conn):
         self.conn = conn
 
-    def get(self, id : int) -> Post:
+    def get(self, id: int) -> Post:
         raise NotImplementedError()
 
     def upsert(self, *posts : list[Posts]) -> list[Posts]:
@@ -22,5 +21,5 @@ class PostRepo:
     def find(self) -> list[Posts]:
         raise NotImplementedError()
 
-    def delete(self, id : int) -> Post:
+    def delete(self, id: int) -> Post:
         raise NotImplementedError()
